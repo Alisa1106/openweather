23e7220ec0da338136d4f2704785134ee04e9193
Alisa_Harodnik <alisa_harodnik@epam.com>
2021-09-01 17:35:52 +0300

    Changed annotation in BaseTest

--- src/test/java/org/openweather/tests/BaseTest.java
+++ src/test/java/org/openweather/tests/BaseTest.java
@@ -3,14 +3,14 @@ package org.openweather.tests;
 import org.openweather.client.HttpClient;
 import org.openweather.client.WeatherService;
 import org.openweather.utils.EnvironmentsReader;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 
     HttpClient httpClient;
     protected WeatherService weatherService;
 
-    @BeforeTest
+    @BeforeClass
     public void initTest() {
         httpClient = new HttpClient(EnvironmentsReader.getEnvironment("QA_ENV"));
         weatherService = new WeatherService(httpClient);
