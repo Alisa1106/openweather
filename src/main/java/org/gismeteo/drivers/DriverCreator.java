d170a7561d05590395f7f8f5fc5aee27ca99ab9c
Alisa_Harodnik <alisa_harodnik@epam.com>
2021-09-16 16:32:59 +0300

    Fixed

--- src/main/java/org/gismeteo/drivers/DriverCreator.java
+++ src/main/java/org/gismeteo/drivers/DriverCreator.java
@@ -5,7 +5,12 @@ import org.gismeteo.utils.Properties;
 public class DriverCreator {
 
     public static WebDriverCreator create() {
+<<<<<<< HEAD
         switch (new Properties().getBrowser()) {
+=======
+        String browser = new Properties().getBrowser();
+        switch (browser) {
+>>>>>>> a028bac (Fixed)
             case "firefox":
                 return new FirefoxDriverCreator();
             case "remote":
